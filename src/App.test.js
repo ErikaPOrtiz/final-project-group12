1cf4e9dfe194fc3456541f471823bd635e43c068
ErikaPOrtiz <erika0418p@gmail.com>
2025-04-09 11:50:02 -0600

    Build and test stage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Group 12, Erika Ortiz ID 456980', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Group 12, Erika Ortiz ID 456980/i);
   expect(linkElement).toBeInTheDocument();
 });
